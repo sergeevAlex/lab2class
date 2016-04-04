d8d731af8e4323e66e618acb67604e2c9e5f2f0e
sergeevAlex <alexsergeev@i.ua>
2016-04-04 16:22:37 +0300

    added text message

--- workclassesoper/Program.cs
+++ workclassesoper/Program.cs
@@ -7,6 +7,13 @@ namespace workclassesoper
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+
+			for(int i = 0; i <20;i++)
+			{
+
+				string st = "Github is on";
+				Console.WriteLine (st);
+			}
 		}
 	}
 }
