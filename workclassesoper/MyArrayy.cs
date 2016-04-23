de5e048650788c8606e39fb6a2f81de2a0f1036d
sergeevAlex <alexsergeev@i.ua>
2016-04-23 14:24:01 +0300

    regroup project
    Class in other file & some changes from keyboard

---
+++ workclassesoper/MyArrayy.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classString
+{
+	public class MyArray : ICloneable
+	{
+
+		private int firstIndex, lastIndex;
+		string[] arr;
+
+		public MyArray(int firstIndex, int lastIndex)
+		{
+
+			if (firstIndex >= lastIndex)
+				throw new ArgumentException("первый индекс должен быть меньше последнего");
+			this.firstIndex = firstIndex; // инициализируем границы
+			this.lastIndex = lastIndex;
+			arr = new string[lastIndex - firstIndex + 1];
+		}
+
+		public int FirstIndex
+		{
+			get { return firstIndex; }
+		}
+
+		public int LastElemPos
+		{
+
+			get { return arr.Length + firstIndex; }
+		}
+
+		public string this[int pos]
+		{
+			get
+			{
+				if (pos < firstIndex || pos > lastIndex)
+					throw new ArgumentOutOfRangeException("Вышли за границы массива");
+				else
+					return arr[pos - firstIndex];
+			}
+			set
+			{
+				if (pos < firstIndex || pos > lastIndex)
+					throw new ArgumentOutOfRangeException("Вышли за границы массива :(");
+				else
+					arr[pos - firstIndex] = value;
+			}
+		}
+
+
+		public object Clone() // клонирование
+		{
+			MyArray copy = new MyArray(this.firstIndex, this.lastIndex);
+			for (int i = 0; i < copy.arr.Length; i++)
+			{
+				copy.arr[i] = this.arr[i];
+			}
+			return copy;
+		}
+
+
+
+
+
+		public static MyArray Concat(MyArray arr1, MyArray arr2)  //объединяем массивы
+		{
+			MyArray temp1 = (MyArray)arr1.Clone();
+			MyArray temp2 = (MyArray)arr2.Clone();
+
+			int temp = temp1.arr.Length;
+			Array.Resize(ref temp1.arr, temp1.arr.Length + temp2.arr.Length);
+			temp1.lastIndex += temp2.arr.Length;
+			temp2.arr.CopyTo(temp1.arr, temp);
+			return temp1;
+
+		}
+
+
+		public static MyArray Merge(MyArray pArr1, MyArray pArr2)
+		{
+			MyArray arr1 = (MyArray)pArr1.Clone();
+			MyArray arr2 = (MyArray)pArr2.Clone();
+
+			int sizeForAdd = arr2.arr.Length;
+			for (int i = 0; i < arr1.arr.Length; i++)
+			{
+				for (int j = 0; j < arr2.arr.Length; j++)
+				{
+					if (arr1.arr[i] == arr2.arr[j]) // если нашли совпадения, повторяющемуся эл-ту присваиваем null
+					{
+						arr2.arr[j] = null;
+						sizeForAdd--;
+					}
+				}
+			}
+
+			int temp = arr1.arr.Length;
+			Array.Resize(ref arr1.arr, arr1.arr.Length + sizeForAdd);
+
+			for (int i = 0; i < arr2.arr.Length; i++)
+			{
+				if (arr2.arr[i] != null)
+				{
+					arr1.arr[temp] = arr2.arr[i];
+					temp++;
+				}
+			}
+
+			arr1.lastIndex += sizeForAdd;
+			return arr1;
+
+		}
+
+	}
+
+}
--- workclassesoper/Program.cs
+++ workclassesoper/Program.cs
@@ -3,189 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace lab2class
+namespace classString
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 50);
+            Console.WriteLine("Задайте левую границу первого массива");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Задайте правую границу первого массива");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Задайте левую границу второго массива");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Задайте правую границу второго массива");
+            int d = int.Parse(Console.ReadLine());
+            
+            MyArray arr = new MyArray(a, b);
+            MyArray arr2 = new MyArray(c, d);
 
-	public class MyArray : ICloneable 
-	{
-
-		private int firstIndex, lastIndex;
-		string[] arr;  
-
-		public MyArray(int firstIndex, int lastIndex) 
-		{
-
-			if (firstIndex >= lastIndex)
-				throw new ArgumentException("первый индекс должен быть меньше последнего");
-			this.firstIndex = firstIndex; // инициализируем границы
-			this.lastIndex = lastIndex;
-			arr = new string[lastIndex - firstIndex +1]; 
-		}
-
-		public int FirstIndex 
-		{
-			get { return firstIndex; }
-		}
-
-		public int LastElemPos 
-		{
-
-			get { return arr.Length + firstIndex; }
-		}
-
-		public string this[int pos] 
-		{
-			get
-			{
-				if (pos < firstIndex || pos > lastIndex) 
-					throw new ArgumentOutOfRangeException("Вышли за границы массива");
-				else
-					return arr[pos-firstIndex];
-			}
-			set
-			{
-				if (pos < firstIndex || pos > lastIndex) 
-					throw new ArgumentOutOfRangeException("Вышли за границы массива :(");
-				else
-					arr[pos-firstIndex] = value;
-			}
-		}
+            for (int i = arr.FirstIndex; i < arr.LastElemPos; i++)
+            {
+                arr[i] = "строка массива " + i;
+            }
+            for (int i = arr2.FirstIndex; i < arr2.LastElemPos; i++)
+            {
+                arr2[i] = "строка массива " + i;
+            }
+            Console.WriteLine("Все элементы первого массива");
+            for (int i = arr.FirstIndex; i < arr.LastElemPos; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
 
 
-		public object Clone() // клонирование
-		{
-			MyArray copy = new MyArray(this.firstIndex, this.lastIndex);
-			for (int i = 0; i < copy.arr.Length; i++)
-			{
-				copy.arr[i] = this.arr[i];
-			}
-			return copy;
-		}
+            Console.WriteLine();
+            Console.WriteLine("Все элементы второго массива");
+            for (int i = arr2.FirstIndex; i < arr2.LastElemPos; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
 
 
+            Console.WriteLine();
 
+            MyArray concatArr = MyArray.Concat(arr, arr2);
+            Console.WriteLine("Объединенный массив");
 
+            for (int i = concatArr.FirstIndex; i < concatArr.LastElemPos; i++)
+            {
+                Console.WriteLine(concatArr[i]);
+            }
+            Console.WriteLine();
 
-		public static MyArray Concat(MyArray arr1, MyArray arr2) //объединяем массивы
-		{
-			MyArray temp1 = (MyArray)arr1.Clone();
-			MyArray temp2 = (MyArray)arr2.Clone();
+            MyArray MergeArr = MyArray.Merge(arr, arr2);
 
-			int temp = temp1.arr.Length;
-			Array.Resize(ref temp1.arr, temp1.arr.Length + temp2.arr.Length);
-			temp1.lastIndex += temp2.arr.Length;
-			temp2.arr.CopyTo(temp1.arr, temp);
-			return temp1;
+            Console.WriteLine("объединенный массив с уникальными элементами");
 
-		}
-
-
-		public static MyArray Merge(MyArray pArr1, MyArray pArr2)
-		{
-			MyArray arr1 = (MyArray)pArr1.Clone();
-			MyArray arr2 = (MyArray)pArr2.Clone();
-
-			int sizeForAdd = arr2.arr.Length;
-			for (int i = 0; i < arr1.arr.Length; i++)
-			{
-				for (int j = 0; j < arr2.arr.Length; j++)
-				{
-					if (arr1.arr[i] == arr2.arr[j]) // если нашли совпадения, повторяющемуся эл-ту присваиваем null
-					{
-						arr2.arr[j] = null;
-						sizeForAdd--;
-					}
-				}
-			}
-
-			int temp = arr1.arr.Length;
-			Array.Resize(ref arr1.arr, arr1.arr.Length + sizeForAdd);
-
-			for (int i = 0; i < arr2.arr.Length; i++)
-			{
-				if (arr2.arr[i] != null)
-				{
-					arr1.arr[temp] = arr2.arr[i];
-					temp++;
-				}
-			}
-
-			arr1.lastIndex += sizeForAdd;
-			return arr1;
-
-		}
-
-	}
-
-
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.SetWindowSize(80, 50);
-			MyArray arr = new MyArray(-3, 6);
-			MyArray arr2 = new MyArray(4, 8);
+            for (int i = MergeArr.FirstIndex; i < MergeArr.LastElemPos; i++)
+            {
+                Console.WriteLine(MergeArr[i]);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Введите индекс строки первого массива к которой вы хотите обратиться: ");
+            int ind = int.Parse(Console.ReadLine());
+            Console.WriteLine("Строка: {0}", arr[ind]);  //обращение к строке массива
+            Console.WriteLine();
 
 
 
-			for (int i = arr.FirstIndex; i < arr.LastElemPos; i++)
-			{
-				arr[i] = "строка массива " + i;
-			}
+            // **границы задавать + по индексу обращение
 
-			for (int i = arr2.FirstIndex; i < arr2.LastElemPos; i++)
-			{
-				arr2[i] = "строка массива " + i;
-			}
+            Console.ReadLine();
 
-			Console.WriteLine("Все элементы первого массива");
-			for (int i = arr.FirstIndex; i < arr.LastElemPos; i++)
-			{
-				Console.WriteLine(arr[i]);
-			}
-
-
-			Console.WriteLine();
-			Console.WriteLine("Элемент с индексом 4 первого массива - {0}", arr[4]);  //обращение к строке массива
-			Console.WriteLine();
-			// границы задавать + по индексу обращение
-			/*
-			Console.WriteLine();
-			Console.WriteLine("Элемент с индексом 5 второго массива - {0}", arr2[5]);
-			Console.WriteLine();
-
-*/
-
-			Console.WriteLine();
-			Console.WriteLine("Все элементы второго массива");
-			for (int i = arr2.FirstIndex; i < arr2.LastElemPos; i++)
-			{
-				Console.WriteLine(arr2[i]);
-			}
-
-
-			Console.WriteLine ();
-
-			MyArray concatArr = MyArray.Concat(arr, arr2);
-			Console.WriteLine("Объединенный массив");
-
-			for (int i = concatArr.FirstIndex; i < concatArr.LastElemPos; i++)
-			{
-				Console.WriteLine(concatArr[i]);
-			}
-			Console.WriteLine();
-
-			MyArray MergeArr = MyArray.Merge(arr, arr2);
-
-			Console.WriteLine("объединенный массив с уникальными элементами");
-
-			for (int i = MergeArr.FirstIndex; i < MergeArr.LastElemPos; i++)
-			{
-				Console.WriteLine(MergeArr[i]);
-			}
-
-			Console.ReadLine();
-
-		}
-	}
+        }
+    }
 }
